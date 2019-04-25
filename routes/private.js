f1af525bebaf59991b42f14a2081b650ff04092b
Harhao <huahaoworkspace@163.com>
2019-04-25 23:05:57 +0800

    add fruit-client

--- routes/private.js
+++ routes/private.js
@@ -2,6 +2,4 @@ const User = require("../controller/user.js");
 const router = require("koa-router")({
   prefix: "/fruit/api/v1"
 });
-router.post("/login", User.login);
-router.post("/register", User.register);
-module.exports = router;
+router.get("/getuserinfo", User.getuserinfo);
